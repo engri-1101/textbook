2b6b569a704c4edcfcdad80b0d4c92e3773f9caa
Henry Robbins <hwr26@cornell.edu>
2020-12-05 16:34:46 -0500

    Added bash script to update all distribution files.
    Former-commit-id: 584911d2d79ca3a3b85c5bfd09c3edeb84b36050

---
+++ update_distros.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+zip () {
+    cd "$1"
+    echo $PWD
+    python zip.py
+    echo 'zipped successfully.'
+    cd ..
+}
+
+zip tsp_integer_programming
+zip maximum_flow
+zip branch_and_bound
+zip baseball_elimination
+zip min-cost_flow
+zip shortest_path
+zip seat_packing
+zip travelling_salesman_problem
+zip first_year_writing_seminar

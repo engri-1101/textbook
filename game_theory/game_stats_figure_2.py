27275cd29b7a40fe36a167adfa770c160309dd62
KylekG <kkg35@cornell.edu>
2021-06-24 10:02:36 -0400

    penalty_kick_game Stats Figure_2 Added Comments and Docstrings, improved code strings
    Added Comments and docstrings to game_stats_figure_2.py, in order to improve readability of the code.
    
    Also improved the code strings used by the figure, by changing unnecessary vars to lets and consts, and improving the conciseness of code.
    
    
    Former-commit-id: 9857a8e84cc10c30b8dd34c311c8fc0d0b69a48b

--- game_theory/game_stats_figure_2.py
+++ game_theory/game_stats_figure_2.py
@@ -2,28 +2,27 @@ from bokeh.plotting import figure
 from bokeh.models import (CustomJSHover, ColumnDataSource, HoverTool)
 
 #<editor-fold Custom JSHover fig_2_xs Code String:
+#Returns the iteration number and makes the necessary changes for highlighting
+#the hovered data point.
 fig_2_xs_code = """
-var index = special_vars.index;
+const index = special_vars.index;
+const data = game_stats_figure_2_source.data;
 
-for(var i = 0;
-    i < game_stats_figure_2_source.data['highlight_alphas'].length;
-    i++){
-    game_stats_figure_2_source.data['highlight_alphas'][i] = 0;
-}
-game_stats_figure_2_source.data['highlight_alphas'][index] = 1;
+data['highlight_alphas'] = new Array(data['highlight_alphas'].length).fill(0);
+data['highlight_alphas'][index] = 1;
 game_stats_figure_2_source.change.emit();
 
-return(game_stats_figure_2_source.data['xs'][index].toString())
+return(data['xs'][index].toString());
 """
 #</editor-fold>
 #<editor-fold Custom JSHover fig_2_ys Code String:
+#Returns the y value of the hovered data point.
 fig_2_ys_code = """
-var index = special_vars.index;
-
-return(game_stats_figure_2_source.data['ys'][index].toString())
+return(game_stats_figure_2_source.data['ys'][special_vars.index].toString());
 """
 #</editor-fold>
 #<editor-fold Custom HoverTool Tooltip Code String:
+#Code below is for how the custom HoverTool displays the information.
 fig_2_custom_tooltip = """
 <div>
     <span style='font-size: 10px;'>Iteration:</span>
@@ -38,6 +37,14 @@ fig_2_custom_tooltip = """
 
 #<editor-fold Stats_fig_2_configs:
 class Stats_fig_2_configs:
+    """Objects of this class are used to organize and pass parameters to
+    Stats Figure 2. All arguments are mutable, and default values for them are
+    the currently decided values being used to make the game. The main purpose
+    of the configurability provided through the use of this class is to make it
+    easier to test changes. Any changes that improve the game should be made
+    directly to the default values of the arguments in this class after
+    successful testing.
+    """
     def __init__(self, figure_base_tools = "box_zoom, wheel_zoom, pan",
                  figure_toolbar_location = "below",
                  figure_toolbar_sticky = False,
@@ -86,7 +93,25 @@ class Stats_fig_2_configs:
 
 #<editor-fold stats_figure_2_setup:
 def stats_figure_2_setup(fig_configs):
+    """Fully creates and sets up Stats Figure 2 for use in the main game. Stats
+    Figure 2 displays the player's score over the iterations played.
+
+
+    Keyword Argument:
+
+    fig_configs - An oject of type Stats_fig_2_configs containing the user's
+    desired figure values within its attributes.
+
+
+    Returns:
+
+    game_stats_figure_2 - The Game stats Bokeh figure displaying the data.
+
+    game_stats_figure_2_source - The ColumnDataSource used by
+    game_stats_figure_2.
+    """
     #<editor-fold Figure Creation:
+    #Create and configure the main aspects of the figure:
     game_stats_figure_2 = figure(tools = fig_configs.figure_base_tools,
                                  toolbar_location = fig_configs.figure_toolbar_location,
                                  toolbar_sticky = fig_configs.figure_toolbar_sticky,
@@ -105,6 +130,7 @@ def stats_figure_2_setup(fig_configs):
     game_stats_figure_2.background_fill_color = fig_configs.figure_background_color
     #</editor-fold>
     #<editor-fold ColumnDataSource Creation:
+    #Create initial values for game_stats_figure_2_source
     source_xs = []
     source_ys = []
     source_heights = []
@@ -115,6 +141,8 @@ def stats_figure_2_setup(fig_configs):
         source_ys.append(0)
         source_heights.append(100)
         source_highlight_alphas.append(0)
+
+    #Create game_stats_figure_2_source with the values that were created.
     source_data = dict(xs = source_xs,
                        ys = source_ys,
                        heights = source_heights,
@@ -122,6 +150,7 @@ def stats_figure_2_setup(fig_configs):
     game_stats_figure_2_source = ColumnDataSource(data = source_data)
     #</editor-fold>
     #<editor-fold Plot Figure Points:
+    #Create the data points for the figure.
     game_stats_figure_2.circle_dot('xs', 'ys',
                                    source = game_stats_figure_2_source,
                                    size = fig_configs.plot_dot_size,
@@ -129,6 +158,7 @@ def stats_figure_2_setup(fig_configs):
                                    fill_color = fig_configs.plot_dot_color)
     #</editor-fold>
     #<editor-fold Plot Figure Highlight Points:
+    #Plot Highlight Points For Figure:
     game_stats_figure_2.circle_dot('xs', 'ys',
                                    source = game_stats_figure_2_source,
                                    size = fig_configs.plot_highlight_dot_size,
@@ -137,23 +167,29 @@ def stats_figure_2_setup(fig_configs):
                                    alpha = 'highlight_alphas')
     #</editor-fold>
     #<editor-fold Plot Invisible Hitboxes:
-    game_stats_figure_2.rect(x = 'xs', y = 0,
-                             source = game_stats_figure_2_source,
-                             width = 1, height = 'heights',
-                             fill_color = fig_configs.plot_dot_color, alpha = 0)
+    #Create the invisible hitboxes for the figure:
+    hbs = game_stats_figure_2.rect(x = 'xs', y = 0,
+                                   source = game_stats_figure_2_source,
+                                   width = 1, height = 'heights',
+                                   fill_color = fig_configs.plot_dot_color,
+                                   alpha = 0)
     #</editor-fold>
     #<editor-fold CustomJSHover Creation:
+    #Create the CustomJSHovers used to format the data for the figure's
+    #custom HoverTool:
     hover_args = dict(game_stats_figure_2_source = game_stats_figure_2_source)
     fig_2_xs_custom = CustomJSHover(code = fig_2_xs_code, args = hover_args)
     fig_2_ys_custom = CustomJSHover(code = fig_2_ys_code, args = hover_args)
     #</editor-fold>
     #<editor-fold Create HoverTool:
+    #Create the Custom HoverTool and add it to the figure:
     hover_formatter = { '@xs' : fig_2_xs_custom,
                         '@ys' : fig_2_ys_custom}
     game_stats_figure_2.add_tools(HoverTool(tooltips = fig_2_custom_tooltip,
                                             formatters = hover_formatter,
                                             mode = "mouse",
-                                            point_policy = "follow_mouse"))
+                                            point_policy = "follow_mouse",
+                                            renderers = [hbs]))
     #</editor-fold>
     return (game_stats_figure_2, game_stats_figure_2_source)
 #</editor-fold>

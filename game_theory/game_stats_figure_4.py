699db07a187686c349b4e6be6a759cb99709d859
KylekG <kkg35@cornell.edu>
2021-06-28 09:11:17 -0400

    penalty_kick_game Game Stats Figure 4 Plotting Improvements
    Added more configuration options for the plot points in game stats figure 4, and added lines that show the possible values of points on the figure. The guiding lines were also given names that will be used in a future custom HoverTool to better describe the data to the user.
    
    
    Former-commit-id: eef000d51d2f3468a9f7d71472efc77c653761d9

--- game_theory/game_stats_figure_4.py
+++ game_theory/game_stats_figure_4.py
@@ -1,6 +1,28 @@
 from bokeh.plotting import figure
 from bokeh.models import (CustomJSHover, ColumnDataSource, HoverTool)
 
+score_probabilities = {'Right' : {'LeftLeft' : 0.55,
+                                  'LeftMiddle' : 0.65,
+                                  'LeftRight' : 0.93,
+                                  'MiddleLeft' : 0.74,
+                                  'MiddleMiddle' : 0.60,
+                                  'MiddleRight' : 0.72,
+                                  'RightLeft' : 0.95,
+                                  'RightMiddle' : 0.73,
+                                  'RightRight' : 0.70},
+                       'Left' :  {'LeftLeft' : 0.67,
+                                  'LeftMiddle' : 0.70,
+                                  'LeftRight' : 0.96,
+                                  'MiddleLeft' : 0.74,
+                                  'MiddleMiddle' : 0.60,
+                                  'MiddleRight' : 0.72,
+                                  'RightLeft' : 0.87,
+                                  'RightMiddle' : 0.65,
+                                  'RightRight' : 0.61}};
+footedness_dicts = ['Right', 'Left']
+aim_directions = ['Left', 'Middle', 'Right']
+goalie_actions = ['Left', 'Middle', 'Right']
+
 class Stats_fig_4_configs:
     """Objects of this class are used to organize and pass parameters to
     Stats Figure 2. All arguments are mutable, and default values for them are
@@ -23,7 +45,12 @@ class Stats_fig_4_configs:
                  figure_xgrid_line_color = None,
                  figure_ygrid_line_color = None,
                  figure_outline_line_color = None,
-                 figure_background_color = "white"):
+                 figure_background_color = "white",
+                 plot_dot_line_color = "#D8CB2D",
+                 plot_dot_fill_color = "#ECDF41",
+                 plot_dot_size = 5, plot_dot_alpha = 1,
+                 guiding_line_color = "#000000",
+                 guiding_line_alpha = 0.1):
         #<editor-fold figure:
         self.figure_base_tools = figure_base_tools
         self.figure_toolbar_location = figure_toolbar_location
@@ -41,8 +68,13 @@ class Stats_fig_4_configs:
         self.figure_ygrid_line_color = figure_ygrid_line_color
         self.figure_outline_line_color = figure_outline_line_color
         self.figure_background_color = figure_background_color
+        self.plot_dot_line_color = plot_dot_line_color
+        self.plot_dot_fill_color = plot_dot_fill_color
+        self.plot_dot_size = plot_dot_size
+        self.plot_dot_alpha = plot_dot_alpha
+        self.guiding_line_color = guiding_line_color
+        self.guiding_line_alpha = guiding_line_alpha
         #</editor-fold>
-
 def stats_figure_4_setup(fig_configs):
     game_stats_figure_4 = figure(tools = fig_configs.figure_base_tools,
                                  toolbar_location = fig_configs.figure_toolbar_location,
@@ -61,7 +93,7 @@ def stats_figure_4_setup(fig_configs):
     game_stats_figure_4.outline_line_color = fig_configs.figure_outline_line_color
     game_stats_figure_4.background_fill_color = fig_configs.figure_background_color
 
-    #Create initial values for game_stats_figure_2_source
+    #Create initial values for game_stats_figure_4_source
     source_xs = []
     source_ys = []
     #Fill the Lists
@@ -69,12 +101,30 @@ def stats_figure_4_setup(fig_configs):
         source_xs.append(i)
         source_ys.append(0)
 
-    #Create game_stats_figure_2_source with the values that were created.
+    #Create game_stats_figure_4_source with the values that were created.
     source_data = dict(xs = source_xs,
                        ys = source_ys)
 
     game_stats_figure_4_source = ColumnDataSource(data = source_data)
 
-    game_stats_figure_4.line('xs', 'ys', source = game_stats_figure_4_source,
-                             line_color = "black")
+    #Plot data points:
+    game_stats_figure_4.circle_dot('xs', 'ys',
+                                   source = game_stats_figure_4_source,
+                                   size = fig_configs.plot_dot_size,
+                                   alpha = fig_configs.plot_dot_alpha,
+                                   line_color = fig_configs.plot_dot_line_color,
+                                   fill_color = fig_configs.plot_dot_fill_color)
+
+    #Plot guiding lines:
+    for foot in footedness_dicts:
+        for direction in aim_directions:
+            for action in goalie_actions:
+                y_val = score_probabilities[foot][direction + action]
+                game_stats_figure_4.line('xs', y_val,
+                                         line_color = fig_configs.guiding_line_color,
+                                         source = game_stats_figure_4_source,
+                                         name = (foot + "," + direction + ","
+                                                 + action),
+                                         line_alpha = fig_configs.guiding_line_alpha)
+
     return game_stats_figure_4, game_stats_figure_4_source

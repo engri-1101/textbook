d083ec98924754a9177059afbc7ea03a80ed38d2
Kristi Zhang <kz226@cornell.edu>
2021-07-13 20:55:46 -0500

    renamed min-cut lab and created zip.py file
    Former-commit-id: b13fefb64ff904c1cb2652bea4324854427252c5

---
+++ minimum_cut/zip.py
@@ -0,0 +1,19 @@
+from zipfile import ZipFile
+import os
+import glob
+
+def write_dir(zipObj, path):
+    '''Write everything in the directory path to the zip object.'''
+    for file in [y for x in os.walk(path) for y in glob.glob(os.path.join(x[0],'*'))]:
+        zipObj.write(file)
+
+
+# Lab
+with ZipFile('minimum_cut_lab.zip', 'w') as zipObj:
+    zipObj.write('minimum_cut_lab.ipynb')
+    zipObj.write('max_flow.py')
+    write_dir(zipObj, 'images-lab')
+    write_dir(zipObj, 'images-key')
+    write_dir(zipObj, 'data-lab')
+    zipObj.close()
+    

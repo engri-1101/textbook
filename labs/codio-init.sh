865fb4ab4e94024ed10cc383dea54530285811d5
Henry Robbins <hwr26@cornell.edu>
2022-07-26 14:01:58 -0400

    Updated link in codio-init.sh script.

--- labs/codio-init.sh
+++ labs/codio-init.sh
@@ -5,9 +5,9 @@
 # rename .guides to .guides_tmp to avoid overlap with pulled .guides file
 mv .guides .guides_tmp
 
-# checkout only the desired lab 
+# checkout only the desired lab
 git init
-git remote add -f origin git@github.coecis.cornell.edu:hwr26/engri-1101-labs.git
+git remote add -f origin git@github.com:engri-1101/textbook.git
 git config core.sparseCheckout true
 echo "labs/$1" >> .git/info/sparse-checkout
 echo "labs/.guides" >> .git/info/sparse-checkout
